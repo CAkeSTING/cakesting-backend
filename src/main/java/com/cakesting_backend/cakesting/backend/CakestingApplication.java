cb295c056449838469daff4548d3193d2c783387
skfkgla <narahim.lee@gmail.com>
2025-01-13 19:55:28 +0900

    Initial commit

---
+++ src/main/java/com/cakesting_backend/cakesting/backend/CakestingApplication.java
@@ -0,0 +1,13 @@
+package com.cakesting_backend.cakesting.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cakesting_backend/cakesting/backend/CakestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakesting_backend.cakesting.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
