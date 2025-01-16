d8531080cd3b5b65dbcfeac893841e3cb78060c8
skfkgla <narahim.lee@gmail.com>
2025-01-16 17:59:26 +0900

    java에서 코틀린으로 언어 전환을 위한 커밋

---
+++ src/main/kotlin/com/cakesting/backend/CackstingBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.cakesting.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CackstingBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CackstingBackendApplication>(*args)
+}
